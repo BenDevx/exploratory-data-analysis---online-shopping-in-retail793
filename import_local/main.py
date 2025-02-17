56f46c684cf8296af44725ce22be40de150e96f0
Ben Omoruyi <benomoruyi@Bens-MacBook-Pro.local>
2025-02-17 12:31:36 +0000

    Removed mistakingly pushed files from repository

--- import_local/main.py
+++
@@ -1,6 +0,0 @@
-from module_1 import function_1
-function_1()
-from module_1 import function_1, function_2
-function_2()
-from module_2 import function_1
-function_1()
--- third_party.py
+++
@@ -1,4 +0,0 @@
-import pandas
-import pandas as pd
-print(f"Pandas version: {pd.__version__}")
-
